7bf26142e3e02d834e2ec04e07e82f2d3e150647
Andy Knolla <andy@techtonicgroup.com>
2017-04-24 17:51:50 -0600

    built pages via controller generator

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+    @posts = Blog.all   # Blog.all is the model. this line grabs all the posts, and stores in "instance variable "posts". Also, anything that happens here, including this model request, is available to the view (as in instance variable)
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
+
+# anything in each of these actions will be run when the matching route is hit. ie pages/home will run 'home' code
