eb386b7bb5b4ad32667cf3f684a0625f6c72a120
Andy Knolla <andy@techtonicgroup.com>
2017-04-24 18:07:15 -0600

    integrated skills via model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all   # Blog.all is the model. this line grabs all the posts, and stores in "instance variable "posts". Also, anything that happens here, including this model request, is available to the view (as in instance variable)
+    @skills = Skill.all
   end
 
   def about
