2469313c40fd101ecb382cb6f46d80a11418e1b6
Andy Knolla <andyknolla@gmail.com>
2017-05-02 13:52:42 -0600

    integrated db relationship between topics and blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
 
   validates_presence_of :title, :body
+
+  belongs_to :topic
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170502192832) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
@@ -21,9 +22,10 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "updated_at",             null: false
     t.string   "slug"
     t.integer  "status",     default: 0
+    t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
+    t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
   end
-
 
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +42,13 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-
   end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
