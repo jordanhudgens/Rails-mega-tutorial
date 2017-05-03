181f3839e9fb735b6a8cb860bb1dbc737fd9c80d
Andy Knolla <andyknolla@gmail.com>
2017-05-03 10:05:55 -0600

    add custom route for individual portfolio, and add friendlyId/Slug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
+
+  # customize route for an individual portfolio
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'porfolio_show'
   resources :blogs
 
   # default...
