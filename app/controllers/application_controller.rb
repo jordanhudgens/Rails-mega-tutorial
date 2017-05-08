fb63540d21848f3dc6e31bfb2170a427395b14af
Andy Knolla <andyknolla@gmail.com>
2017-05-08 10:48:21 -0600

    Moved devise params into controller concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    # specific to Rails 5
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  include DeviseWhitelist
 end
 
 # all other controllers inherit from this controller.
