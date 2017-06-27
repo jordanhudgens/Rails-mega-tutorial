e6d0e2f0561dd4deeb13e2594bff5fd0b6903f6f
Jordan Hudgens <jordan@wow.com>
2017-06-27 16:24:52 -0700

    Added method check to the blog model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,9 +2,12 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
-  attr_accessor :slug
 
   validates_presence_of :title, :body
 
   belongs_to :topic
+
+  def should_generate_new_friendly_id?
+    new_record? || slug.blank?
+  end
 end
