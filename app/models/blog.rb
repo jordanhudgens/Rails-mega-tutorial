eee9b02225440a6f9903bf468a0fc355e02bca49
Andy Knolla <andyknolla@gmail.com>
2017-06-26 20:15:03 -0600

    attr_accessor slug in blog.rb

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,8 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
+
+  attr_accessor :slug
   validates_presence_of :title, :body
 
   belongs_to :topic
