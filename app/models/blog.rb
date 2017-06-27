eac0e3ecd6a9eb03852455c6e3d42352829e57ab
Jordan Hudgens <jordan@wow.com>
2017-06-27 16:10:50 -0700

    Added an attr_accessor to the model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,7 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+  attr_accessor :slug
 
   validates_presence_of :title, :body
 
